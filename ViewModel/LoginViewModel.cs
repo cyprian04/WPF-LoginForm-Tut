698d491b1d5ae1407885c219b4b7da10fc7f4ea4
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-08-17 14:42:45 +0200

    Implemented LoginViewModel.cs + comments

---
+++ ViewModel/LoginViewModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security;
+using System.Windows.Input;
+
+namespace Wpf_LoginForm_Tut.ViewModel
+{
+    public class LoginViewModel: ViewModelBase
+    {
+        //Fields
+        private string _username;
+        private SecureString _password;
+        private string _errorMessage;
+        private bool _isVisible = true;
+
+        public string Username 
+        {
+            get =>_username; // shortcut(lambda) instead of get{}... .We can use it only if we have one thing
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username)); // passing whole property as parameter, not variable directly
+            }
+        }
+
+        
+        public SecureString Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+
+        public string ErrorMessage
+        {
+            get => _errorMessage;
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged(nameof(ErrorMessage));
+            }
+        }
+
+
+        public bool IsVisible
+        {
+            get => _isVisible;
+            set
+            {
+                _isVisible = value;
+                OnPropertyChanged(nameof(IsVisible));
+            }
+        }
+
+        //Commands
+        public ICommand LoginCommand { get; }
+        public ICommand ShowPasswordCommand { get; }
+        public ICommand RecoverPasswordCommand { get; }
+        public ICommand RememberPasswordCommand { get; }
+
+        //Ctors
+
+        public LoginViewModel()
+        {
+            LoginCommand = new ViewModelCommand(ExecuteLoginCommand, CanExecuteLoginCommand);
+            RecoverPasswordCommand = new ViewModelCommand(p => ExecuteRecoverPasswordCommand("",""));
+        }
+
+        private bool CanExecuteLoginCommand(object obj)
+        {
+            bool validData;
+            if (string.IsNullOrWhiteSpace(Username) || Username.Length < 3 || Password == null || Password.Length < 3)
+                validData = false;
+            else
+                validData = true;
+
+            return validData;
+        }
+
+        private void ExecuteLoginCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ExecuteRecoverPasswordCommand(string username, string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
