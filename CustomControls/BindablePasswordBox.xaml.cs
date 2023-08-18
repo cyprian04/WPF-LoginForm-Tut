39f8bd903df1687257c6b7ead5e3826192c271db
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-08-18 10:35:05 +0200

    Implemented BindablePasswordBox partial class

--- CustomControls/BindablePasswordBox.xaml.cs
+++ CustomControls/BindablePasswordBox.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Wpf_LoginForm_Tut.CustomControls
 {
     /// <summary>
@@ -20,11 +22,23 @@ namespace Wpf_LoginForm_Tut.CustomControls
     /// </summary>
     public partial class BindablePasswordBox : UserControl
     {
+        public static readonly DependencyProperty PasswordProperty = DependencyProperty.Register("Password", typeof(SecureString), typeof(BindablePasswordBox));
 
+        public SecureString Password
+        {
+            get { return (SecureString)GetValue(PasswordProperty); }
+            set { SetValue(PasswordProperty, value); }
+        }
 
         public BindablePasswordBox()
         {
             InitializeComponent();
+            txtPassword.PasswordChanged += OnPasswordChanged;
+        }
+
+        private void OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            Password = txtPassword.SecurePassword;
         }
     }
 }
